c230182d99573b4fb4792dc438b5b46aebded5bf
pororobro <pororobro@naver.com>
2021-07-08 15:18:37 +0900

    master

---
+++ study/keras/keras01.py
@@ -0,0 +1,18 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+x = np.array([1,2,3])
+y = np.array([1,2,3])
+
+model = Sequential()
+model.add(Dense(1, input_dim=1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(x,y, epochs=1000, batch_size=1)
+
+loss = model.evaluate(x.y)
+print('loss: ', loss)
+
+result = model.predict([4])
+print('4의 예측값: ',result)
